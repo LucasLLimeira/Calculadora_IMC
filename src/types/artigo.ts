5b2438418ed8d9fecbc9662a00066c34ede85229
Lucas De Lucena Limeira <146790429+LucasLLimeira@users.noreply.github.com>
2026-04-15 19:49:28 -0300

    Delete src directory

--- src/types/artigo.ts
+++
@@ -1,9 +0,0 @@
-export type Artigo = {
-  _id: string;
-  title: string;
-  slug: string;
-  author: string;
-  date: string; // ISO date string, e.g. "2025-10-01"
-  excerpt: string;
-  content: string;
-};
